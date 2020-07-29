5a104551d992e2dbfe3f9a220149287ed307e545
rad-a <11688089+rad-a@users.noreply.github.com>
2020-07-28 22:12:04 -0500

    updating seeds and schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,13 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers (
-    id INT AUTO_INCREMENT,
-    burger_name VARCHAR(30),
+CREATE TABLE burgers
+(
+    id INT
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (100),
     devoured BOOLEAN,
-    PRIMARY KEY (id)
+    PRIMARY KEY
+    (id)
 );
